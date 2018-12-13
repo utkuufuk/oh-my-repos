9b142f45d99b1d300980d9b9510c2286d90b3a2c
utkuufuk <utkuufuk@gmail.com>
2018-12-13 17:55:45 +0300

    fix a bug while copying the config file

--- install.py
+++ install.py
@@ -5,15 +5,19 @@ from pathlib import Path
 from shutil import copyfile
 from subprocess import call, STDOUT
 
-CONFIG_PATH = str(Path.home()) + '/.config/oh-my-repos/config.json'
+CONFIG_DIR = str(Path.home()) + '/.config/oh-my-repos'
 config = {'dir':str(Path.home()) + '/Github'}
 try:
     copyfile("./pull.py", "/usr/bin/pull")
     print("Script copied into /usr/bin/")
     call('chmod +x /usr/bin/pull'.split(), stderr=STDOUT, stdout = open(os.devnull, 'w')) == 0
-    with open(CONFIG_PATH, 'w', encoding='utf-8') as f:
+    try:
+        os.mkdir(CONFIG_DIR)
+    except FileExistsError:
+        pass
+    with open(CONFIG_DIR + "/config.json", 'w', encoding='utf-8') as f:
         json.dump(config, f, indent=4, ensure_ascii=False)
-        print("Configuration file created:", CONFIG_PATH)
+        print("Configuration file created:", CONFIG_DIR + "/config.json")
     print("Installation successful.")
 except PermissionError:
     print("Permission denied. Try running the script again with 'sudo'")
