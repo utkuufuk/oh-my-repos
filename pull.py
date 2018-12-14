e878e320fdbcdeab5b046016a8e287249f989417
utkuufuk <utkuufuk@gmail.com>
2018-12-14 17:25:43 +0300

    maintain an extensible set of git root dirs

--- pull.py
+++ pull.py
@@ -10,26 +10,52 @@ TIMEOUT_SECS = 15
 CONFIG_PATH = str(Path.home()) + "/.oh-my-repos.json"
 
 if __name__ == '__main__':
-    # read configuration file
-    try:
-        config = json.load(open(CONFIG_PATH))
-    except FileNotFoundError:
-        print('Config file not found at', CONFIG_PATH + ', try reinstalling.', file=sys.stderr)
-        sys.exit(1)
-
-    # read program argument if specified
+    # read program arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dir', type=str, default=config['dir'], help='Root directory of Git repositories.')
+    parser.add_argument('--dir', type=str, help='Root directory of Git repositories.')
+    parser.add_argument('--save', action='store_true',
+                        help='Whether to add the input directory to the existing set of saved directories.')
     args = parser.parse_args()
+
+    # get saved directories from configuration file if any
+    savedDirs = set()
+    try:
+        savedDirs = {s for s in json.load(open(CONFIG_PATH))['dir'] if s != ""}
+    except FileNotFoundError:
+        print("Configuration file not found.")
+
+    if args.dir == None:
+        if args.save:
+            raise ValueError("--save flag can only be used along with the --dir argument")
+        if len(savedDirs) == 0:
+            print("Could not find any saved directories. Using the current directory as Git Root.")
+            savedDirs.add(os.getcwd())
+    else:
+        # convert input directory to absolute path & add it to the set of retrieved directories
+        inputDir = os.path.abspath(args.dir.replace("~", str(Path.home())))
+        savedDirs.add(inputDir)
+        if args.save:
+            # add the input directory to the existing set of saved directories in the configuration file
+            with open(CONFIG_PATH, 'w', encoding='utf-8') as f:
+                json.dump({"dir":list(savedDirs)}, f, indent=4, ensure_ascii=False)
+                print("Input directory appended to existing directories in configuration file:", inputDir)
 
     # remember the current directory
     initialDir = os.getcwd()
+    try:
+        # delete .mrconfig if exists
+        os.remove(str(Path.home()) + "/.mrconfig")
+    except OSError:
+        pass
 
     # discover & register repositories
-    repoDirs = [f.path for f in os.scandir(args.dir) if f.is_dir() and os.path.isdir(f.path + "/.git")]
-    for repo in repoDirs:
-        os.chdir(repo)
-        call(['mr', 'register'], stderr=STDOUT, stdout = open(os.devnull, 'w'))
+    for gitRoot in savedDirs:
+        print("\nDiscovering repositories in", gitRoot)
+        repoDirs = [f.path for f in os.scandir(gitRoot) if f.is_dir() and os.path.isdir(f.path + "/.git")]
+        for repo in repoDirs:
+            os.chdir(repo)
+            call(['mr', 'register'], stderr=STDOUT, stdout = open(os.devnull, 'w'))
+            print("  ", repo)
 
     # run myrepos command from user home directory
     os.chdir(str(Path.home()))
